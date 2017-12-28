46dd913068e646c3f3f32a76d2af894d34607e8e
GarrisonWang <34771379+GarrisonWang@users.noreply.github.com>
2017-12-28 19:01:17 +0000

    Add files via upload

---
+++ Assets/example8.7/newbranch3.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newbranch3 : MonoBehaviour {
+
+    public void growbranches3(int index)
+    {
+        this.transform.position += this.transform.up * 2 * this.transform.localScale.y;
+    }
+}
