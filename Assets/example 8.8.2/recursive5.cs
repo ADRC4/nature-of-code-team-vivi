7c6d5efa881d79f719573641b64b017d164ea561
GarrisonWang <34771379+GarrisonWang@users.noreply.github.com>
2017-12-28 19:00:43 +0000

    Add files via upload

---
+++ Assets/example 8.8.2/recursive5.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recursive5 : MonoBehaviour
+{
+
+    public int recursetime = 5;
+    int spreadbranchamount;
+
+    private void Start()
+    {
+        spreadbranchamount = Random.Range(1, 4);
+        recursetime -= 1;
+
+        for (int i = 0; i < spreadbranchamount; i++)
+        {
+            if (recursetime > 0)
+            {
+                var copy = Instantiate(gameObject);
+                var branch = copy.GetComponent<recursive5>();
+                branch.SendMessage("growbranches5", i);
+            }
+        }
+    }
+}
---
+++ Assets/example 8.8.2/scalebranch5.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scalebranch5 : MonoBehaviour {
+
+    public void growbranches5(int index)
+    {
+        this.transform.localScale *= 0.7f;
+    }
+}
