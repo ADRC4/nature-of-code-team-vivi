1bc79f4753a050ecc2561886d6b5fec586cd4ba5
lipinkie <34612033+lipinkie@users.noreply.github.com>
2017-12-30 21:36:21 +0000

    Example3.2
    example3.2

---
+++ Assets/Example302/Script/RectControl1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectControl1 : MonoBehaviour {
+
+    float speed = 1f;
+    //float timeMaxToZero = 3f;
+    //float maxSpeed = 3f;
+
+    //float accelRatePerSec;
+    //float forwardVelocity;
+
+    float screenHalfWidthInWorldUnits;
+
+	void Start ()
+    {
+        //accelRatePerSec = maxSpeed / timeMaxToZero;
+        //forwardVelocity = 0f;
+
+        //float halfRectWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+        //float inputX = Input.GetAxisRaw("Horizontal");
+        //float velocity = inputX * speed;
+        //transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if(transform .position .x < -screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y );
+        }
+        if (transform.position.x > screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+
+    }
+}
