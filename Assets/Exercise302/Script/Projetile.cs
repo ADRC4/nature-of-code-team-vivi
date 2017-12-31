8f943c9d83052da1495e6cc7b4990cf3b3b2df0f
lipinkie <34612033+lipinkie@users.noreply.github.com>
2017-12-31 00:32:31 +0000

    Exercise3.2
    Exercise3.2

---
+++ Assets/Exercise302/Script/Projetile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetile : MonoBehaviour {
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
+    }
+
+    //   float speed = 10;
+    //   public void SetSpeed(float newSpeed)
+    //   {
+    //       speed = newSpeed;
+    //   }
+    //// Update is called once per frame
+    //void Update ()
+    //   {
+    //       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    //}
+}
---
+++ Assets/Exercise302/Script/ShootCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCube : MonoBehaviour
+{
+    public GameObject objectPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newObject = Instantiate(objectPrefab) as GameObject;
+            newObject.transform.position = transform.position;
+
+            Rigidbody rb = newObject.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * 20;
+        }
+	}
+}
