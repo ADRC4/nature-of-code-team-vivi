8633839e4b85ffa6be8746fdde5aef61e238ea77
lipinkie <34612033+lipinkie@users.noreply.github.com>
2018-01-03 17:09:50 +0000

    Exercise 3.13
    Exercise 3.13
    What is the magnitude of the normal force?

---
+++ Assets/Exercise313/WhatIsTheMagnitudeOfTheNormalForce.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatIsTheMagnitudeOfTheNormalForce : MonoBehaviour
+{
+    //theta = angle theta;
+    // Fnormal = Mathf.Cos(theta) * Fgravity;
+}
