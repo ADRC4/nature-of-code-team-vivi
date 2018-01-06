c572c7b99bc9c7a08583057dc6a1dda3984b7ef6
stephanie-hui <34771957+stephanie-hui@users.noreply.github.com>
2018-01-06 23:36:11 +0000

    Add files via upload

---
+++ Assets/exercise3.8/scripts/DrawLines.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour {
+
+    private LineRenderer line;
+
+    public Transform point0;
+    private Transform point1;
+    //public Transform point2;
+    //public Transform point3;
+    public int points;
+
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = points + 1;
+        line.useWorldSpace = false;
+        //line.SetPosition(0, point0.position);
+
+    }
+
+
+    void Update()
+    {
+        point1 = GameObject.Find("Sphere(Clone)").transform;
+        Vector3 pointA = point0.position;
+        Vector3 pointB = point1.position;
+        //point1.position = transform.position.
+        line.SetPosition(0, point1.position);
+
+        //Vector3 pointC = point0.position;
+        //Vector3 pointD = point2.position;
+        //line.SetPosition(1, point2.position);
+
+        //Vector3 pointA = point0.position;
+        //Vector3 pointF = point3.position;
+        //line.SetPosition(1, point3.position);
+    }
+}
---
+++ Assets/exercise3.8/scripts/SphereWithAccel.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereWithAccel : MonoBehaviour {
+    bool animate = true;
+    //float Period = 2 * Mathf.PI;
+    //float Speed = 2f;
+    //float amplitude;
+    //float amplitude = 2f;
+    private GameObject Qiu;
+    float theta = 0;
+    float Period = 2 * Mathf.PI;
+    float aVelocity = 2f;
+    float aAcceleration = 0.01f;
+    float amplitude = 2f;
+    int numberOfObjects = 6;
+    private Transform point1;
+
+    GameObject[] Qius;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Qiu == null)
+        {
+            Qiu = GameObject.CreatePrimitive(PrimitiveType.Sphere);//create a sphere
+            Qius = new GameObject[numberOfObjects];
+
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                Qius[i] = (GameObject)GameObject.Instantiate(Qiu, new Vector3(transform.position.x, 0, 0), Quaternion.identity);
+                //point1 = GameObject.Find("Sphere(Clone)").transform;
+            }
+            Qiu.SetActive(false);//hide the original
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animate)
+        {
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                if (i < 3)
+                {
+                    float a = i * Period / numberOfObjects;
+                    //a += aVelocity;
+                    //amplitude = Random.Range(2, 5);
+                    //aVelocity = Random.Range(0f, 0.1f);
+                    //amplitude = Random.Range(-4f, 4f);
+                    a += Time.time * aVelocity;
+                    float y = Mathf.Sin(a) * amplitude;
+                    //float x = theta;
+                    //float y = amplitude * Mathf.Sin(theta);
+                    aVelocity += aAcceleration;
+                    //float x = amplitude * Mathf.Sin(theta);
+                    //float y = amplitude * Mathf.Sin(theta);
+
+                    NewMethod(i).transform.position = new Vector3(transform.position.x, y, 0);
+                }
+                else
+                {
+                    float a = i * Period / numberOfObjects;
+                    a += Time.time * aVelocity;
+                    float y = Mathf.Sin(a) * amplitude;
+                    NewMethod(i).transform.position = new Vector3(-transform.position.x, y, 0);
+                    //Debug.Log(i);
+                }
+            }
+        }
+    }
+
+    private GameObject NewMethod(int i)
+    {
+        return Qius[i];
+    }
+}
