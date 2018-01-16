cff8c4b4d1441a9096d35068a2225a9c7d30bfd2
FengzeJia <34771643+FengzeJia@users.noreply.github.com>
2018-01-16 08:53:18 +0000

    ...

--- Assets/Chapter8/Example 8.2/Use Prefab/NewTest.cs
+++ Assets/Chapter8/Example 8.2/Use Prefab/NewTest.cs
@@ -7,14 +7,13 @@ public class NewTest : MonoBehaviour
 {
     public Transform prefab;
 
-    float nextPosition = 0;
+    float nextPosition;
     float NewPosition, PrevPosition;
     float nextRadius;
     float diameter = 5f;
     float FirstMove;
     //scale the instantiated circle to half
     Vector3 shrinkCircle = new Vector3(0.5f, 0.5f, 0);
-
 
     void Start()
     {
@@ -27,18 +26,33 @@ public class NewTest : MonoBehaviour
 
         for (int i = 1; i < 4; i++)
         {
-            Vector3 pos = new Vector3();
-            var instantiatedPrefab_Right = Instantiate(prefab, pos, Quaternion.identity);
-            instantiatedPrefab_Right.transform.localScale = shrinkCircle;
-        }
-          
+            //int j = Math.Pow(2, n);
 
+            int j = 2 ^ i;
+
+            Vector3 Pos1 = new Vector3(diameter * 0.5f, 0, 0);
+            var instantiatedPrefab_Right = Instantiate(prefab, Pos1, Quaternion.identity);
+            instantiatedPrefab_Right.transform.localScale = shrinkCircle/j;
+
+            /*
+             Vector3 Pos1 = new Vector3(diameter * 0.5f, 0, 0);
+            var instantiatedPrefab_Right = Instantiate(prefab, Pos1, Quaternion.identity);
+            instantiatedPrefab_Right.transform.localScale = shrinkCircle;
+
+            Vector3 Pos2 = new Vector3(diameter * 0.75f, 0, 0);
+            var instantiatedPrefab_Right2 = Instantiate(prefab, Pos2, Quaternion.identity);
+            instantiatedPrefab_Right2.transform.localScale = shrinkCircle / 2;
+
+            Vector3 Pos3 = new Vector3(diameter * 0.875f, 0, 0);
+            var instantiatedPrefab_Right3 = Instantiate(prefab, Pos3, Quaternion.identity);
+            instantiatedPrefab_Right3.transform.localScale = shrinkCircle / 4;
+            */
+            //instantiatedPrefab_Right.transform.position;
+
+            //var instantiatedPrefab_Left = Instantiate(prefab, -Pos, Quaternion.identity);
+            // instantiatedPrefab_Left.transform.localScale = shrinkCircle;
+        }
         //var instantiatedPrefab_Left = Instantiate(prefab, new Vector3(nextPosition - radius / 2, 0, 0), Quaternion.identity);
         //instantiatedPrefab_Left.transform.localScale = shrinkCircle;
-
-
     }
-
-
-
 }
