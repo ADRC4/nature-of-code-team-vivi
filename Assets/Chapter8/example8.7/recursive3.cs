fc57be110ba08d9dc2fcf545fde5019b59a24f65
wang Garrison <echobravoecho@gmail.com>
2018-01-16 16:36:14 +0000

    reupload the work after clean

---
+++ Assets/Chapter8/example8.7/recursive3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recursive3 : MonoBehaviour {
+
+    public int recursetime = 5;
+    public int spreadbranchamount = 2;
+
+    private void Start()
+    {
+        recursetime -= 1;
+
+        for (int i = 0; i < spreadbranchamount; i++)
+        {
+            if (recursetime > 0)
+            {
+                var copy = Instantiate(gameObject);
+                var branch = copy.GetComponent<recursive3>();
+                branch.SendMessage("growbranches3", i);
+            }
+        }
+    }
+}
---
+++ Assets/Chapter8/exercise 8.12 and example 8.10/LsystemTree.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LsystemTree : MonoBehaviour
+{//23
+    private string axiom = "F";
+    private string current;
+    private float angle;
+
+
+    private Stack<TransformInfo> TransformStack = new Stack<TransformInfo>();
+    private Dictionary<char, string> rules = new Dictionary<char, string>();
+
+    private float length=0.08f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rules.Add('F', "FF+[+F-F-F]-[-F+F+F]");
+        current = axiom;
+        angle = 25f;
+
+
+        Maketree();
+        Maketree();
+        Maketree();
+        Maketree();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Maketree()
+
+    {
+        string newstring = "";
+
+        char[] stringcharacters = current.ToCharArray();
+
+        for (int i = 0; i < stringcharacters.Length; i++)
+        {
+            char currentcharacter = stringcharacters[i];
+
+
+            if (rules.ContainsKey(currentcharacter)) { newstring += rules[currentcharacter]; }
+            else { newstring += currentcharacter.ToString(); }
+
+
+
+        }
+
+        current = newstring;
+
+        Debug.Log(current);
+
+
+        stringcharacters = current.ToCharArray();
+
+        for (int i = 0; i < stringcharacters.Length; i++)
+        {
+            char currentcharacter = stringcharacters[i];
+
+            if (currentcharacter == 'F')
+            {
+                //forwards
+
+                Vector3 start = transform.position;
+                transform.Translate(Vector3.up * length);
+
+                Debug.DrawLine(start, transform.position, Color.black, 10000f, false);
+
+
+            }
+
+            else if (currentcharacter == '+')
+            {
+                transform.Rotate(Vector3.forward* angle*-1f);
+
+            }
+
+            else if (currentcharacter == '-')
+            {
+                transform.Rotate(Vector3.forward* -angle*-1f);
+
+            }
+
+            else if (currentcharacter == '[')
+            {
+                TransformInfo ti = new TransformInfo();
+                ti.position = transform.position;
+                ti.rotation = transform.rotation;
+                TransformStack.Push(ti);
+            }
+
+            else if (currentcharacter == ']')
+            {
+                TransformInfo ti = TransformStack.Pop();
+                transform.position = ti.position;
+                transform.rotation = ti.rotation;
+
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+}
