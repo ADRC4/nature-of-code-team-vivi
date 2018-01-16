e341f5f5c7ed9fb933ee811b6f8d7afaa84c5600
FengzeJia <34771643+FengzeJia@users.noreply.github.com>
2018-01-16 13:22:09 +0000

    ...

--- Assets/Chapter8/Example 8.2/Use Prefab/NewTest.cs
+++ Assets/Chapter8/Example 8.2/Use Prefab/NewTest.cs
@@ -12,6 +12,9 @@ public class NewTest : MonoBehaviour
     float nextRadius;
     float diameter = 5f;
     float FirstMove;
+    float x;
+    
+
     //scale the instantiated circle to half
     Vector3 shrinkCircle = new Vector3(0.5f, 0.5f, 0);
 
@@ -25,15 +28,15 @@ public class NewTest : MonoBehaviour
     {
         //int max = 3;
 
-        for (int i = 1; i < 10; i++)
+        for (int n = 3; n < 10; n++)
         {
-             //float Scale = 1/2;
-             Vector3 Pos1 = new Vector3(diameter * (1/2 ^ i), 0, 0);
-
-            //Pos1.x=diameter*[(1/2)^1+(1/2)^2+(1/2)^3+... ...+(1/2)^i];
+            //Summation formula of geometric sequence/ Instantiated circles' position
+            x = diameter * ((1/2)^n-1);
+            
+            Vector3 Pos1 = new Vector3(x, 0, 0);
 
             var instantiatedPrefab_Right = Instantiate(prefab, Pos1, Quaternion.identity);
-             instantiatedPrefab_Right.transform.localScale = shrinkCircle / (2 ^ i);
+             instantiatedPrefab_Right.transform.localScale = shrinkCircle / (2 ^ n);
         
             /*
              Vector3 Pos1 = new Vector3(diameter * 0.5f, 0, 0);
