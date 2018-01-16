269b742de04e1ab05673dc450ee74dfb2d8fed3d
wang Garrison <echobravoecho@gmail.com>
2018-01-16 16:42:35 +0000

    reupload of my work after clean the folder

--- Assets/Chapter8/example 8.8/newbranch4.cs
+++ Assets/Chapter8/example 8.8/newbranch4.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class newbranch4 : MonoBehaviour {
-
+    //
     public void growbranches4(int index)
     {
         this.transform.position += this.transform.up * 2 * this.transform.localScale.y;
